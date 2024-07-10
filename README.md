78459938a86da42cfe5c22d98324ad9dcbd8b775
Bill Pun <bill@Bills-MacBook-Air.local>
2024-07-09 20:43:20 -0700

    updated schema with offer id

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 	1. install visual studio code.
 	2. install the SQLite Viewer plugin.
 - To simulate, run `python simulate.py`
-- To generate the schema plot, please follow the installation instruction in `schema_plot.py` and run `python schema_plot.py`.
+- To generate the schema plot, please follow the installation instruction in `schema.py` and run `python schema.py`.
--- entities.py
+++ entities.py
@@ -37,9 +37,8 @@ class Application(Base):
     visitor_id: Mapped[str] = mapped_column(ForeignKey('visitors.visitor_id'))
     conversion_flag: Mapped[bool] = mapped_column(default=False) # necessary if we have status?
 
-    # missing these columns?
+    # ! need to know what the application is about
     campaign_id: Mapped[str] = mapped_column(ForeignKey('campaigns.campaign_id'))
-    channel_id: Mapped[str] = mapped_column(ForeignKey('channels.channel_id'))
 
     def __repr__(self):
         return f'ap_{self.application_id}_{self.status_dt.strftime(utils.DATETIME_FORMAT)}'
@@ -48,25 +47,32 @@ class Application(Base):
 class TargetPopulation(Base):
 
     __tablename__ = 'target_population'
-    # should there be an cus_id?
 
-    visitor_id: Mapped[str] = mapped_column(ForeignKey('visitors.visitor_id'), primary_key=True)
-    # don't we need the columns below?
     campaign_id: Mapped[str] = mapped_column(ForeignKey('campaigns.campaign_id'), primary_key=True)
-    channel_id: Mapped[str] = mapped_column(ForeignKey('channels.channel_id'), primary_key=True)
+    visitor_id: Mapped[str] = mapped_column(ForeignKey('visitors.visitor_id'), primary_key=True)
+
+    offer_id: Mapped[str] = mapped_column()
 
     resp_prob: Mapped[float] = mapped_column(default=0.0) # soft pull
     conv_prob: Mapped[float] = mapped_column(default=0.0) # hard pull
     cus_value: Mapped[float] = mapped_column(default=0.0) # null for prospect?
 
     # ! this is great for simulation (but could be huge)
+    # this implies that even if the visitor is ineligible, statistics are still available
     eligibility_flag: Mapped[bool] = mapped_column(default=False)
 
     # what is this?
     expected_approval_rate: Mapped[float] = mapped_column(default=0.0)
 
+    # ! this could just be a target dt depending on how often this is refreshed
+    # period in which the target population and statistics are valid
+    valid_from: Mapped[datetime] = mapped_column(nullable=True)
+    valid_to: Mapped[datetime] = mapped_column(nullable=True)
+
+    # offer id
+    # solicitation attributes
     def __repr__(self):
-        return f'tp_{self.visitor_id}_{self.spend_id}'
+        return f'tp_{self.visitor_id}_{self.campaign_id}'
 
 # campaign?
 class MarketingSpend(Base):
@@ -75,16 +81,18 @@ class MarketingSpend(Base):
 
     # !!!!! depends on if campaign_id encodes channel
     campaign_id: Mapped[str] = mapped_column(ForeignKey('campaigns.campaign_id'), primary_key=True) # same ID across channels?
-    channel_id: Mapped[str] = mapped_column(ForeignKey('channels.channel_id'), primary_key=True)
+    # channel_id: Mapped[str] = mapped_column(ForeignKey('channels.channel_id'), primary_key=True)
+    start_dt: Mapped[datetime] = mapped_column(primary_key=True) # what does it refer to?
+    end_dt: Mapped[datetime] = mapped_column(primary_key=True) # what does it refer to?
 
     # how does channel spend come about at the campaign level?
     channel_spend: Mapped[float] = mapped_column(default=0.0)
     campaign_spend: Mapped[float] = mapped_column(default=0.0)
-    start_dt: Mapped[datetime] = mapped_column(nullable=True) # what does it refer to?
-    end_dt: Mapped[datetime] = mapped_column(nullable=True) # what does it refer to?
 
     def __repr__(self):
-        return f'ms_{self.spend_id}'
+        return f'ms_{self.campaign_id}_' + \
+            f'{self.start_dt.strftime(utils.DATETIME_FORMAT)}_' + \
+            f'{self.end_dt.strftime(utils.DATETIME_FORMAT)}'
 
 class Channel(Base):
 
@@ -103,6 +111,12 @@ class Campaign(Base):
 
     campaign_id: Mapped[str] = mapped_column(primary_key=True)
     campaign_name: Mapped[str] = mapped_column(default='')
+
+    # a campaign is created for a channel
+    channel_id: Mapped[str] = mapped_column(ForeignKey('channels.channel_id'))
+
+    # a campaign is about selling a product
+    product_id: Mapped[str] = mapped_column(ForeignKey('products.product_id'))
 
     # ! may need additional attribution for similarity
 
@@ -127,7 +141,46 @@ class Customer(Base):
 
     customer_id: Mapped[str] = mapped_column(primary_key=True)
 
-    # ! need additional attribution for similarity
+    # ! would an indicator be enough or 
+    # do they have their own customer id
+    card_member_ind: Mapped[bool] = mapped_column()
+    dpl_ind: Mapped[bool] = mapped_column()
 
+    # ! need additional attribution for similarity
     def __repr__(self):
         return f'u_{self.customer_id}'
+
+class Product(Base):
+
+    __tablename__ = 'products'
+
+    product_id: Mapped[str] = mapped_column(primary_key=True)
+    product_name: Mapped[str] = mapped_column()
+
+    headline_rate_lb: Mapped[float] = mapped_column(default=0)
+    headline_rate_ub: Mapped[float] = mapped_column(default=0)
+
+class CustomerProducts(Base):
+
+    __tablename__ = 'customer_products'
+
+    customer_id: Mapped[str] = mapped_column(ForeignKey('customers.customer_id'), primary_key=True)
+    product_id: Mapped[str] = mapped_column(ForeignKey('products.product_id'), primary_key=True)
+
+    # active, cancelled, paid off
+    status: Mapped[str] = mapped_column()
+
+    start_dt: Mapped[datetime] = mapped_column()
+    end_dt: Mapped[datetime] = mapped_column()
+
+    # ! what additional attributes do we need?
+
+# class Offers(Base):
+
+#     __tablename__ = 'offers'
+
+#     offer_id: Mapped[str] = mapped_column(primary_key=True)
+#     valid_from: Mapped[datetime] = mapped_column()
+#     valid_to: Mapped[datetime] = mapped_column()
+
+
