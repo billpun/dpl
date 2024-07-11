da18bfbd19f1e54f8ac55367d18be4279fbdea59
Bill Pun <bill@Bills-MacBook-Air.local>
2024-07-10 22:57:08 -0700

    updated with questions in notes

--- entities_ana.py
+++ entities_ana.py
@@ -169,6 +169,8 @@ class Application(Base):
     updated_dt: Mapped[datetime] = mapped_column(primary_key=True)
     status: Mapped[str] = mapped_column()
     visitor_key: Mapped[int] = mapped_column(ForeignKey('visitors.visitor_key'))
+    campaign_id: Mapped[str] = mapped_column(ForeignKey('campaigns.campaign_id'))
+    offer_id: Mapped[str] = mapped_column(ForeignKey('offers.offer_id'))
 
 class MarketSpendHistory(Base):
 
